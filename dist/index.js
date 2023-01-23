38e088913606c38a9a9979c4468448438fdc1827
Gideon Addo <gideonaddoofori@gmail.com>
2023-01-23 15:46:50 +0000

    Change the whole code.

--- dist/index.js
+++ dist/index.js
@@ -6,6 +6,22 @@ const getElement = (selected) => {
   );
 };
 
+const linksContainer = getElement(".links");
+const alertText = getElement(".alert_message");
+const input = getElement(".url-value");
+
+const fadeIn = [
+  { opacity: "0", scale: "0.95" },
+  { opacity: "1", scale: "1" },
+];
+
+const fadeInTiming = {
+  duration: 200,
+  iterations: 1,
+  fill: "forwards",
+  easing: "ease-in-out",
+};
+
 const API_URL = "https://api.shrtco.de/v2/";
 
 const openMenu = () => {
@@ -16,18 +32,14 @@ const openMenu = () => {
 };
 
 const displayAlertMessage = () => {
-  const alertText = getElement(".alert_message");
   alertText.textContent = "Please add a link";
   alertText.classList.add("show");
-  const input = getElement(".url-value");
   input.classList.add("alert");
 };
 
 const removeAlertMessage = () => {
-  const alertText = getElement(".alert_message");
   alertText.textContent = "";
   alertText.classList.remove("show");
-  const input = getElement(".url-value");
   input.classList.remove("alert");
 };
 
@@ -37,22 +49,19 @@ menuBtn.addEventListener("click", openMenu);
 const form = getElement(".form");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  getUrlFromUser();
-  reset();
+  shortenURL();
 });
 
 const reset = () => (getElement(".url-value").value = "");
 
-const getUrlFromUser = async () => {
+const shortenURL = () => {
   const url = getElement(".url-value").value;
   if (!url) {
     displayAlertMessage();
     return;
   } else {
+    fetchData(url);
     removeAlertMessage();
-    let shortLink = await fetchData(url);
-    addToLocalStorage(url, shortLink);
-    renderLinks(url, shortLink);
   }
 };
 
@@ -61,15 +70,18 @@ const fetchData = async (url) => {
     const result = await fetch(`${API_URL}shorten?url=${url}`);
     const response = await result.json();
     const shortLink = response.result.short_link;
-    return shortLink;
+    const id = new Date().getTime().toString();
+    createLinkItem(url, shortLink, id);
+    addToLocalStorage(url, shortLink, id);
+    reset();
   } catch (err) {
     console.log(err);
     throw new Error("There was an error fetching data");
   }
 };
 
-const addToLocalStorage = (longUrl, shortUrl) => {
-  const links = { longUrl, shortUrl };
+const addToLocalStorage = (longUrl, shortUrl, id) => {
+  const links = { longUrl, shortUrl, id };
   if (!longUrl) {
     return;
   } else {
@@ -79,50 +91,83 @@ const addToLocalStorage = (longUrl, shortUrl) => {
   }
 };
 
-const getLocalStorage = () => {
+function getLocalStorage() {
   return localStorage.getItem("links")
     ? JSON.parse(localStorage.getItem("links"))
     : [];
-};
+}
 
-const setUpLinks = () => {
+function setUpLinks() {
   let links = getLocalStorage();
   links.map((link) => {
-    const { longUrl, shortUrl } = link;
-    renderLinks(longUrl, shortUrl);
+    const { longUrl, shortUrl, id } = link;
+    createLinkItem(longUrl, shortUrl, id);
   });
-};
+}
 
 window.addEventListener("DOMContentLoaded", setUpLinks);
 
-const renderLinks = (longLink, shortLink) => {
+function createLinkItem(longLink, shortLink, id) {
   const links = getElement(".links");
+
   const element = document.createElement("div");
+  element.setAttribute("data-id", id);
   element.setAttribute("class", "links-container");
   const template = `<p class="original-link">${longLink}</p>
             <div class="short_link-copy_btn">
               <p class="shortened-link">https://${shortLink}</p>
-              <button class="copy-link">Copy</button>
+              <div class="button_container">
+                <button class="copy-link">Copy</button>
+                <button class="delete_link"><i class='far fa-trash-alt'></i></button>
+              </div>
             </div>`;
   element.innerHTML = template;
   links.appendChild(element);
+
+  // copy the shortened link
   const copyButton = element.querySelector(".copy-link");
   const shortUrl = element.querySelector(".shortened-link");
-  copyShortLink(copyButton, shortUrl);
-};
-
-const copyShortLink = (copyButton, shortLink) => {
-  copyButton.addEventListener("click", () => {
-    navigator.clipboard.writeText(shortLink.textContent);
-    copyButton.textContent = "Copied!";
-    copyButton.style.backgroundColor = "hsl(257, 27%, 26%)";
-    resetCopyBtn(copyButton);
+  copyButton.addEventListener("click", (e) => {
+    copyShortLink(e, shortUrl);
   });
-};
+
+  //animate element
+  const container = getElement(".links-container");
+  container.animate(fadeIn, fadeInTiming);
+
+  // delete the link
+  const deleteLinkBtn = element.querySelector(".delete_link");
+  deleteLinkBtn.addEventListener("click", deleteLink);
+}
+
+function deleteLink(e) {
+  const element = e.currentTarget.parentElement.parentElement.parentElement;
+  const id = element.dataset.id;
+  linksContainer.removeChild(element);
+  removeFromLocalStorage(id);
+}
+
+function removeFromLocalStorage(id) {
+  let links = getLocalStorage();
+  links = links.filter((link) => link.id !== id);
+  localStorage.setItem("links", JSON.stringify(links));
+}
+
+function copyShortLink(e, shortLink) {
+  const button = e.target;
+  if (navigator.clipboard) {
+    navigator.clipboard.writeText(shortLink.textContent).then(() => {
+      button.textContent = "Copied!";
+      button.style.backgroundColor = "hsl(257, 27%, 26%)";
+
+      setTimeout(() => {
+        resetCopyBtn(button);
+      }, 2000);
+    });
+  }
+}
 
 const resetCopyBtn = (button) => {
-  setTimeout(() => {
-    button.textContent = "Copy";
-    button.style.backgroundColor = "hsl(180, 66%, 49%)";
-  }, 1000);
+  button.textContent = "Copy";
+  button.style.backgroundColor = "hsl(180, 66%, 49%)";
 };
